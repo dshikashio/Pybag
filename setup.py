1157d6bf624d09d4d4bb73b044985275c6b4eae5
Doug Shikashio <doug.shikashio@gmail.com>
2021-08-27 00:39:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Pybag',
-      version='2.0.0',
+      version='2.2.0',
       description='Python wrappers for DbgEng from Windbg',
       author='Doug S',
       url='https://github.com/dshikashio/Pybag',
