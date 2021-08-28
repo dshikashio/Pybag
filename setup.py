311e6d7ad35e85a53919646457d6922768ff6173
Doug Shikashio <doug.shikashio@gmail.com>
2021-08-28 14:42:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(name='Pybag',
-      version='2.2.0',
+      version='2.2.1',
       description='Python wrappers for DbgEng from Windbg',
+      long_description=README,
+      long_description_content_type="text/markdown",
       author='Doug S',
       url='https://github.com/dshikashio/Pybag',
+      license="MIT",
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8",
+      ],      
       packages=['pybag', 'pybag.dbgeng'],
-      package_data={'pybag' : ['dbgeng/tlb/*.tlb']}
-      )
+      package_data={'pybag' : ['dbgeng/tlb/*.tlb']},
+      include_package_data=True,
+      install_requires=[
+          'capstone>=4.0.2',
+          'comtypes>=1.1.10',
+          'pywin32>=301',
+      ]
+ )
