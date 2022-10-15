469866e81114dcc1486daca9a0c0c16ba26d8de7
dougs <doug.shikashio@gmail.com>
2022-10-15 18:12:05 -0400

    update

--- run_tests.bat
+++ run_tests.bat
@@ -1,11 +1,14 @@
-REM discovery doesn't work well due to:
-REM need to split dbgeng instances into different processe
+REM Use auto test discovery
+
+python -m unittest discover
+
+
+REM If discovery doesn't work well:
+REM need to split dbgeng instances into different processes
 REM until its clear how to properly release all references to objects
 
-REM python -m unittest discover
-
-python -m tests.test_idebugclient
-python -m tests.test_target TestBasic
-python -m tests.test_target TestTargetCreate
-python -m tests.test_target TestTargetAttach1
-python -m tests.test_target TestTargetAttach2
+REM python -m tests.test_idebugclient
+REM python -m tests.test_target TestBasic
+REM python -m tests.test_target TestTargetCreate
+REM python -m tests.test_target TestTargetAttach1
+REM python -m tests.test_target TestTargetAttach2
