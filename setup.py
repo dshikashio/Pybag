9fbaae6d799b192a0b379141ef260ddfe6b13f1e
Doug Shikashio <doug.shikashio@gmail.com>
2024-02-22 21:16:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='Pybag',
-      version='2.2.8',
+      version='2.2.9',
       description='Python wrappers for DbgEng from Windbg',
       long_description=README,
       long_description_content_type="text/markdown",
