99fe5bdd9faf47b5f9591c17d61910422148a751
Dan <46821332+nsadeveloper789@users.noreply.github.com>
2024-02-29 01:29:16 +0000

    Improve dbgdir search (#17)
    Allow user to specify it manually via an environment variable.
    Attempt to load the root from the register (as was previously commented)
    Attempt to find the default paths.

--- pybag/__init__.py
+++ pybag/__init__.py
@@ -1,25 +1,43 @@
 import ctypes
 import platform
 import os
+import winreg
 
 __all__ = ['UserDbg', 'KernelDbg', 'CrashDbg', 'DbgEng']
 
-# reg query "HKLM\SOFTWARE\Microsoft\Windows Kits\Installed Roots" /v KitsRoot10
 
-if platform.architecture()[0] == '64bit':
-    dbgdirs = [r'C:\Program Files\Windows Kits\10\Debuggers\x64',
-               r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64']
-else:
-    dbgdirs = [r'C:\Program Files\Windows Kits\10\Debuggers\x86',
-               r'C:\Program Files (x86)\Windows Kits\10\Debuggers\x86']
-dbgdir = None
-for _dir in dbgdirs:
-    if os.path.exists(_dir):
-        dbgdir = _dir
-        break
+def append_arch(dbgroot):
+    if platform.architecture()[0] == '64bit':
+        return os.path.join(dbgroot, r'Debuggers\x64')
+    else:
+        return os.path.join(dbgroot, r'Debuggers\x86')
 
-if not dbgdir:
+
+def find_dbgdir():
+    dbgdir = os.getenv('WINDBG_DIR')
+    if dbgdir is not None and os.path.exists(dbgdir):
+        return dbgdir
+    try:
+        roots_key = winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            r'SOFTWARE\Microsoft\Windows Kits\Installed Roots')
+        dbgroot = winreg.QueryValueEx(roots_key, 'KitsRoot10')[0]
+        dbgdir = append_arch(dbgroot)
+        if os.path.exists(dbgdir):
+            return dbgdir
+    except FileNotFoundError:
+        pass
+    default_roots = [r'C:\Program Files\Windows Kits\10',
+                     r'C:\Program Files (x86)\Windows Kits\10']
+    for dbgroot in default_roots:
+        dbgdir = append_arch(dbgroot)
+        if os.path.exists(dbgdir):
+            return dbgdir
     raise RuntimeError("Windbg install directory not found!")
+
+
+dbgdir = find_dbgdir()
+
 
 # preload these to get correct DLLs loaded
 try:
@@ -32,10 +50,9 @@ ctypes.windll.LoadLibrary(os.path.join(dbgdir, 'dbgeng.dll'))
 
 del platform
 del os
-del _dir
 del dbgdir
-del dbgdirs
 del ctypes
+del winreg
 
 from .pydbg      import DbgEng
 from .crashdbg   import CrashDbg
